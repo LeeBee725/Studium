e8e2ab9eef84ba49c86633616a9aa13e66248c89
lsw725 <725lsw@hanmail.net>
2022-10-15 11:41:46 +0900

    start studium project

---
+++ src/test/java/golmu/studium/StudiumApplicationTests.java
@@ -0,0 +1,13 @@
+package golmu.studium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
